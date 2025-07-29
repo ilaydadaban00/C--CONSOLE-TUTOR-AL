2d0581bdd1e58f3bd1032f90659d08fdc8930bed
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:26:11 +0300

    Add files via upload

---
+++ uygulama-11/uygulama-11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool kontrol1 = true;
+
+            bool kontrol2 = false;
+
+
+
+            bool sonuc = 10 > 5;
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
