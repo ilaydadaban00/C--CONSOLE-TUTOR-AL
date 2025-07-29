90301726dfd27e5c6af17008d4747d504461d064
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:36:37 +0300

    Add files via upload

---
+++ uygulama-13/uygulama-13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 5;            
+            int sayi2 = 15;
+            int sayi3 = 7;
+
+
+            int sonuc1 = sayi1 + sayi2;
+            int sonuc2 = (sayi1 + sayi2) - sayi3;
+
+            Console.WriteLine(sonuc1);
+            Console.WriteLine(sonuc2);
+
+            Console.ReadLine();
+        }
+    }
+}
