1c70cf380f124ce3c83770cb74d96dd82632ac9e
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 12:05:02 +0300

    Add files via upload

---
+++ uygulama-20/uygulama-20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 21;
+            int sayi2 = 34;
+
+            bool kontrol1 = sayi1 <= 40; 
+            bool kontrol2 = sayi2 <= 33;
+
+            Console.WriteLine(kontrol1); 
+            Console.WriteLine(kontrol2);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
