e576fb15c5529277dafe431ff3c9b099a40e8a31
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:10:56 +0300

    Add files via upload

---
+++ uygulama-7/uygulama-7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float ondalikli_sayi1 = 35.56f;   //sonuna f koymamızın sebebi onu double olarak algılamasıdır.
+
+            float ondalikli_sayi2 = 12.5f;
+
+            Console.WriteLine(ondalikli_sayi1);
+            Console.WriteLine(ondalikli_sayi2);
+
+            Console.ReadLine();
+        }
+    }
+}
