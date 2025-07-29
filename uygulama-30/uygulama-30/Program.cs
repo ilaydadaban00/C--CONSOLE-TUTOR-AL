4012e6a405e8c725c3376cbffaac725416ff3315
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 13:30:06 +0300

    Add files via upload

---
+++ uygulama-30/uygulama-30/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sayi1 = "3";
+            string sayi2 = "5";
+            int a = Convert.ToInt32(sayi1);
+            int b = Convert.ToInt32(sayi2);
+
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
