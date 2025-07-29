24c34bfe6b9222fa2efa95c857a86c4dd45c95fc
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 17:52:02 +0300

    Add files via upload

---
+++ uygulama-48/uygulama-48/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hak = 3;
+            while(true)
+                {
+                Console.WriteLine("kullanıcı adınızı giriniz");
+                string kullaniniciAdi = Console.ReadLine();
+                Console.WriteLine(" sifrenizi giriniz");
+                string sifre = Console.ReadLine();
+
+                if(kullaniniciAdi=="ilayda" && sifre == "123")
+                {
+                    Console.WriteLine("tebrikler giriş yaptınız");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("kullanıcı adınız veya şifreniiz hatalı");
+                    if (hak > 0)
+                    {
+                        hak -= 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+
+            }
+        }
+    }
+}
