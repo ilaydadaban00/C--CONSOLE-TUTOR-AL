68348434d636ab0c17dabec4bdd37727d5f915f9
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 14:57:30 +0300

    Add files via upload

---
+++ uygulama-35/uygulama-35/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakiye = 1000;
+            Console.WriteLine("Atm ye hoşgeldiniz");
+            Console.WriteLine("yapmak istediğiniz işlemi seçiniz");
+
+            string secim = Console.ReadLine();
+
+            if(secim=="1")
+            {
+                Console.WriteLine("şuan ki bakiyeniz  :"+bakiye);
+            }
+            else if(secim=="2")
+            {
+                Console.WriteLine("çekmek istediğiniz tutarı giriniz");
+                int cekilecek_tutar =Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("kalan tutar  :" + (bakiye - cekilecek_tutar));
+                
+            }else if (secim == "3")
+            {
+                Console.WriteLine("yatırmak istediginiz mikatrı ekleyiniz");
+                int yatıralacak_tutar = Convert.ToInt32( Console.ReadLine());
+                Console.WriteLine("şuan ki faturanız  :" + yatıralacak_tutar + bakiye);
+
+            }
+            else if(secim=="Q")
+            {
+                Console.WriteLine("ATM ÇIKIŞ YAPILIYOR");
+                Console.WriteLine("ÇIKIŞ YAPILDI İYİ GÜNLLER");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
