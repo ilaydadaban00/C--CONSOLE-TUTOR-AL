ec6de790767a631ad6b57d573fa03bb30de67c78
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 13:36:19 +0300

    Add files via upload

---
+++ uygulama-31/uygulama-31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 5;
+
+            string a = sayi1.ToString();
+            string b = sayi2.ToString();
+
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+
+            double sayi = 5.22;
+            Console.WriteLine(sayi);
+            Console.WriteLine(sayi.GetType());
+            Console.ReadLine();
+        }
+    }
+}
