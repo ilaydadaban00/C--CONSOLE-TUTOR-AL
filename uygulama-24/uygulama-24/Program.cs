9cb2aca774b0d9f4ca9156061e93bb0d59734bc9
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 12:24:04 +0300

    Add files via upload

---
+++ uygulama-24/uygulama-24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +=
+            // -=
+            // /=
+            // *=
+
+            int a = 5;
+
+            //a=a+4;
+            //a+=4;
+            //a=a-3;
+            //a-=3;
+
+            a = a * 3;
+
+            Console.WriteLine("a'nın degeri:" + a);
+            Console.ReadLine();
+
+        }
+    }
+}
