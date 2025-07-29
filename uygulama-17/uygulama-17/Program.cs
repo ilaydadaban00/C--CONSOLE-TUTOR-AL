84420606e10170dcfef8d546e19dd17af056a708
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:52:55 +0300

    Add files via upload

---
+++ uygulama-17/uygulama-17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 4;
+            int sayi2 = 2;
+            int sayi3 = 2;
+
+            bool kontrol2 = sayi3 == sayi2;
+
+            bool kontrol1 = sayi1 == sayi2;
+            Console.WriteLine(kontrol1);
+            Console.WriteLine(kontrol2);
+
+            Console.ReadLine();
+        }
+    }
+}
