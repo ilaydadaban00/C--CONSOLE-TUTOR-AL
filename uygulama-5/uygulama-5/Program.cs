7fae2fd4729f3ded6ca7cff29752a8ce83e4be63
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 10:57:15 +0300

    Add files via upload

---
+++ uygulama-5/uygulama-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int birinciDeger = 150000;
+            int ikinciDeger = -500000;
+            int maxDeger = int.MaxValue;
+            int minDeger = int.MinValue;
+
+
+            Console.WriteLine(birinciDeger);
+            Console.WriteLine(ikinciDeger);
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(minDeger);
+
+            Console.ReadLine();
+        }
+    }
+}
