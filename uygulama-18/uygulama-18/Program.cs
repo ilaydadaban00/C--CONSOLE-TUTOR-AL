8a4aa4d03bedb1ca6b85c82bd3d6991a26f359bd
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:56:04 +0300

    Add files via upload

---
+++ uygulama-18/uygulama-18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 15;
+            int sayi2 = 27;
+            bool kontrol = sayi1 != sayi2;
+            Console.WriteLine(kontrol);
+            Console.ReadLine();
+        }
+    }
+}
