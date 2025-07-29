13ef87739ff2a0455cb15f9f82009262d6b80d30
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 14:15:00 +0300

    Add files via upload

---
+++ uygulama-33/uygulama-33/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("notunu girer misin");
+            string not = Console.ReadLine();
+            int a = Convert.ToInt32(not);
+
+            
+            if (a>= 50)
+            {
+                Console.WriteLine("başarılı");
+            }
+            else
+            {
+                Console.WriteLine("biraz daha çalışmalısın");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
