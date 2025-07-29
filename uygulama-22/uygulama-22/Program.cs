ee44b73cc4de9607366434e982c4d563cc25fb1a
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 12:12:23 +0300

    Add files via upload

---
+++ uygulama-22/uygulama-22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 3;
+            int sayi2 = 3;
+            int sayi3 = 6;
+
+            bool kontrol = sayi1 != sayi2 || sayi2 > sayi3;
+
+            Console.WriteLine(kontrol);
+            Console.ReadLine();
+        }
+    }
+}
