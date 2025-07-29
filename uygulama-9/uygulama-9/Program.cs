31b7cc35b0e9122914d54feb7525552cfa66d62c
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:19:50 +0300

    Add files via upload

---
+++ uygulama-9/uygulama-9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char karakter1 = 'i';  //tek tırnakta içerisinde tek değer alınır.
+
+            char karekter2 = '?';
+
+            Console.WriteLine(karakter1);
+            Console.WriteLine(karekter2);
+
+            Console.ReadLine();
+        }
+    }
+}
