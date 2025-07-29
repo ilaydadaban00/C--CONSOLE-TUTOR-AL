51a3d063b3322b135bb630cceca6e675eefac738
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 15:46:22 +0300

    Add files via upload

---
+++ uygulama-37/uygulama-37/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakiye = 5000;
+
+            Console.WriteLine("Atm hoşgeldiniz...");
+            Console.WriteLine("lütfen seçim yapınız");
+            Console.WriteLine("1- bakiye görüntüleme");
+            Console.WriteLine("2- para çek");
+            Console.WriteLine("3- para yatır");
+            Console.WriteLine("4- çıkış yap");
+
+            string secim = Console.ReadLine();
+
+            switch (secim)
+            {
+                case "1":
+
+                    Console.WriteLine("bakiyeniz" + bakiye);
+                    Console.ReadLine();
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("çekeceğiniz tutatrı girer misiniz");
+                    int paraCek = Convert.ToInt32(Console.ReadLine());
+                    if (paraCek > bakiye)
+                    {
+                        Console.WriteLine("çekeceğiniz tutar bakiyenizden fazla");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("bakiyeniz  " + (bakiye - paraCek));
+                        Console.ReadLine();
+
+                    }
+                    Console.ReadLine();
+
+
+                    break;
+
+
+                case "3":
+                    Console.WriteLine("çekeceğiniz tutatrı girer misiniz");
+                    int parayatır = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("bakiyeniz  " + (bakiye + parayatır));
+                    Console.ReadLine();
+
+
+                    break;
+                case "4":
+
+                    Console.WriteLine("Atm çıkış yapılıyor");
+                    Console.ReadLine();
+
+
+                    break;
+
+
+                default:
+
+                    Console.WriteLine("lütfen geçerli değer giriniz");
+
+                    break;
+
+
+
+            }
+
+
+        }
+    }
+}
