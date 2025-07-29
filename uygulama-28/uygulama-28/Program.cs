3b6b4c15856821768c54e3628b6d51eecd1f15c0
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 13:21:03 +0300

    Add files via upload

---
+++ uygulama-28/uygulama-28/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi1 = "1";
+            string sayi2 = "2";
+
+            Console.WriteLine(sayi1 + sayi2);
+            Console.ReadLine();
+
+
+
+            string sayi1 = "1";
+            string sayi2 = "2";
+
+            int a = int.Parse(sayi1);
+            int b = int.Parse(sayi2);
+
+            Console.WriteLine(sayi1 + sayi2);
+            Console.ReadLine();
+        }
+    }
+}
