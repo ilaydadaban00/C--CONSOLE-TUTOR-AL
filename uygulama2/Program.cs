87d775ec0e1179a33ae3a65e36304603dc1e9678
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 10:26:24 +0300

    Add files via upload

---
+++ uygulama2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ilayda");   //WriteLine ekrana yazdırma için kullanılır alt alta getirir
+            Console.WriteLine("daban");
+
+
+            Console.Write("ilayda");      //Write ekranda gösterir fakat birleştirir
+            Console.Write("daban");
+            Console.ReadLine();           //ReadLine ekranda  sabit olarak tutar. 
+        }
+    }
+}
