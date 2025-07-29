d66375ee680f8aae992736293b40e2688f8d5b0c
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 10:40:01 +0300

    Add files via upload

---
+++ uygulama-3/uygulama-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte birinci = 5;
+
+            byte ikinci = 10;
+            byte ucuncu = 244;
+            byte maxdeger = byte.MaxValue;  //bu değer byte en alabileceği en büyük değeri gösterir.
+            byte mindeger = byte.MinValue;  //bu değer byte en alabileceği en küçğk değeri gösterir.
+
+            Console.WriteLine(birinci);
+            Console.WriteLine(ikinci);
+            Console.WriteLine(ucuncu);
+            Console.WriteLine(maxdeger);
+            Console.WriteLine(mindeger);
+            
+            Console.ReadLine();
+        }
+    }
+}
