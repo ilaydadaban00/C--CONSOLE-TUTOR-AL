ad3a1b7eb3de4bd8a18017ba5e517f78bcc10d05
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 15:13:45 +0300

    Add files via upload

---
+++ uygulama36/uygulama36/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen kilonuzu giriniz");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("lütfen boyunuzu giriniz");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double BedenKitleIndexi = kilo / (boy * boy);
+
+            if (BedenKitleIndexi <= 18)
+            {
+                Console.WriteLine("zayıf");
+                Console.ReadLine();
+
+            }
+            else if(BedenKitleIndexi>18 && BedenKitleIndexi < 25)
+            {
+                Console.WriteLine("normal");
+                Console.ReadLine();
+
+
+            }
+            else if (BedenKitleIndexi > 25)
+            {
+                Console.WriteLine("obez");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
