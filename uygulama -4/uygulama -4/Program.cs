06ccc2c1b83cd7aa0ee556c4a2b8dc145e47dc27
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 10:48:00 +0300

    Add files via upload

---
+++ uygulama -4/uygulama -4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // short değişkeni -32768 ile 32768 arasındaki değerleri alabilir.
+
+            short birinciDegisken = -50;
+
+            short ikinciDegisten = 25550;
+            short ucuncuDegisten = 32000;
+
+            short maxDeger = short.MaxValue;
+            short MinDeger = short.MaxValue;
+
+
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(MinDeger);
+            Console.WriteLine(birinciDegisken);
+            Console.WriteLine(ikinciDegisten);
+            Console.WriteLine(ucuncuDegisten);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
