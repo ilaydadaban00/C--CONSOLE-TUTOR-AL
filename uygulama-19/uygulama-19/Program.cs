1243f438705aeef14cc0bd2f33e57b4f0e2c82d3
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 12:00:07 +0300

    Add files via upload

---
+++ uygulama-19/uygulama-19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int yas1 = 18;
+            int yas2 = 7;
+
+
+            bool kontrol1 = yas1 >= 18;
+            bool kontrol2 = yas2 >= 18;
+
+
+            Console.WriteLine(kontrol1);
+            Console.WriteLine(kontrol2);
+
+            Console.ReadLine();
+        }
+    }
+}
