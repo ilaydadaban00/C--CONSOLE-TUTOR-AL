91e5245a98310b2db086e8526b655ceb8f03dc94
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 16:26:10 +0300

    Add files via upload

---
+++ uygulama-43/uygulama-43/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+
+            }
+            while (a <= 10);
+
+            Console.ReadLine();
+        }
+
+    }
+}
