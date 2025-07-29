dff1e1f36f9a9dd800a9525074797bfdd340f955
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 16:40:57 +0300

    Add files via upload

---
+++ uygulama-44/uygulama-44/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int toplam = 0;
+            do
+            {
+                toplam += a;
+                a++;
+
+            } while (a <= 100);
+            Console.WriteLine(toplam);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
