cfb151242c459402056da78e8825551920ef3e39
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 12:35:37 +0300

    Add files via upload

---
+++ uygulama-26/uygulama-26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen adınızı giriniz.");
+
+            string isim = Console.ReadLine();
+
+
+            Console.WriteLine("soyad giriniz");
+            string soyad = Console.ReadLine();
+
+
+            Console.WriteLine("kullanıcının ismi :" + isim);
+            Console.WriteLine("kullanıcının Soyad :" + soyad);
+
+            Console.ReadLine();
+
+        }
+    }
+}
