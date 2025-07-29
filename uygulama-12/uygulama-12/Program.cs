da7421b91221958e9718c8ffa0ae987cd16558ff
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-29 11:30:49 +0300

    Add files via upload

---
+++ uygulama-12/uygulama-12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object a = true;
+            object b = "ilayda";
+            object c = 24;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
