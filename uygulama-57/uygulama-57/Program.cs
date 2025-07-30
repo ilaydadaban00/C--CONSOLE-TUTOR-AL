b48b06489af0c5c69b7a47d1b94ad0296bb15806
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 12:11:33 +0300

    Add files via upload

---
+++ uygulama-57/uygulama-57/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            toplama(5, 7);
+            carpma(5, 7);
+            isimsoyisim("ilayda", "daban");
+            Console.ReadLine();
+        }
+
+        static void toplama(int sayi1,int sayi2)
+        {
+            Console.WriteLine("gönderilen sayıların toplamı   " + (sayi1 + sayi2));
+              
+        }
+
+
+        static void carpma(int sayi1,int sayi2)
+        {
+            Console.WriteLine("gönderilen değerlerin çarpımı " + (sayi1 * sayi2));
+        }
+        static void isimsoyisim(string deger1,string deger2)
+        {
+            Console.WriteLine("  isim ve soyisim  " + deger1 + "  " + deger2);
+        }
+    }
+}
