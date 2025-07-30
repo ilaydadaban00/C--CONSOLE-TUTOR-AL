e52d1175069cf6ae3e5087f993530586c41da7f0
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 13:46:29 +0300

    Add files via upload

---
+++ uygulama-59/uygulama-59/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isimdondurme = Isim("ilayda", "daban");
+            Console.WriteLine(isimdondurme);
+            Console.ReadLine();
+        }
+        static int toplama(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        static string Isim(string ad,string Soyad)
+        {
+            return ad + " " + Soyad;
+        }
+        
+
+    }
+}
