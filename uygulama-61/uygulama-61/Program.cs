e3250de5049c9cd64fb70b6aa878513dc04e824c
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 14:11:15 +0300

    Add files via upload

---
+++ uygulama-61/uygulama-61/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayı girer misin");
+            int deger = Convert.ToInt32(Console.ReadLine());
+            TekmiCiftmi(deger);
+            Console.ReadLine();
+        }
+        static void TekmiCiftmi(int sayi)
+        {
+            if (sayi % 2 == 1)
+            {
+                Console.WriteLine("sayınız tektir...");
+            }
+            else
+            {
+                Console.WriteLine("sayınız çifftir");
+            }
+        }
+    }
+}
