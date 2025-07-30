3b2416bd0ed0dd3bb8d7d3e89447fe2982461ce7
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 13:04:49 +0300

    Add files via upload

---
+++ uygulama-58/uygulama-58/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int donendeger=   toplamyap(6, 2);
+
+            if (donendeger > 10)
+            {
+                Console.WriteLine("değer 10'dan buyuk");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("değer 10'dan küçük");
+                Console.ReadLine();
+            }
+        }
+        static int toplamyap(int deger1,int deger2)
+        {
+            int sonuc = deger1 + deger2;
+            return sonuc; 
+        }
+
+
+    }
+}
