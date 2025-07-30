efc476d8f860b90dd7c7e1a36f6a0f1dea9d71c6
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 10:16:53 +0300

    Add files via upload

---
+++ uygulama-53/uygulama-53/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = { "ilayda", "rukiye", "melek", "dilan", "beyza" };
+            foreach (var a in isimler)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
