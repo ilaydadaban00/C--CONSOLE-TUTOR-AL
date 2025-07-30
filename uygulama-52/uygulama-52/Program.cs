46ac5b0288d8ea5b901718a346f451638136fed7
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 10:09:32 +0300

    Add files via upload

---
+++ uygulama-52/uygulama-52/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayi = new int[4];
+
+
+            for(int i = 1; i < sayi.Length; i++)
+            {
+                Console.Write("sayılar dizisinin"+(i)+"index değerini giriniz");
+                sayi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("***********************");
+
+            for(int i = 0;i< sayi.Length; i++)
+            {
+                Console.WriteLine(sayi[i]);
+         
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
