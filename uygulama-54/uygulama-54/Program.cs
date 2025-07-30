da6f7815b56db2bb6912b8f4268d23929313626d
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 10:34:59 +0300

    Add files via upload

---
+++ uygulama-54/uygulama-54/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 10, 10, 20, 30, 20, 30, 40, 50, 40,40, 50, 70, 55, 65, 70, 55, 65, 5, 20, 35, 65, 80 };
+
+            int adet = 0;
+
+            Console.WriteLine("sayı girer misin");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayi == sayilar[i])
+                {
+                    adet++;
+                }
+            }
+            if (adet == 0)
+            {
+                Console.WriteLine("girdiğiniz değer bulunamadı");
+            }
+            else
+            {
+                Console.WriteLine("girdiğiniz sayi dizide bulundu...");
+                Console.WriteLine("adet" + adet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
