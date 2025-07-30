385a3c179ced491f25aa43d76913263b368814e1
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 11:59:35 +0300

    Add files via upload

---
+++ uygulama-56/uygulama-56/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SelamVer();
+            SelamVer();
+            SelamVer();
+            SelamVer();
+            SelamVer();
+
+            
+        }
+        static void SelamVer()
+        {
+            Console.WriteLine("selamlar");
+            Console.ReadLine();
+        }
+        static void Ad()
+        {
+            Console.WriteLine("ilayda");
+            Console.ReadLine();
+        }
+
+
+    }
+}
