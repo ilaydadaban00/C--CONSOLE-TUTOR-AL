f74fa206dbab2c7f47f3761a4a83565e8d651114
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 14:03:06 +0300

    Add files via upload

---
+++ uygulama-60/uygulama-60/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen değer giriniz.");
+            int deger = Convert.ToInt32(Console.ReadLine());
+            int kare = kareal(deger);
+            if (kare >= 25)
+            {
+                Console.WriteLine("değeriniz 25 eşit veya büyük");
+            }
+            else
+            {
+                Console.WriteLine("değeriniz küçüktür 25'den");
+            }
+            Console.ReadLine();
+
+        }
+        static int kareal(int a)
+        {
+            return a * a;
+        }
+        
+
+    }
+}
