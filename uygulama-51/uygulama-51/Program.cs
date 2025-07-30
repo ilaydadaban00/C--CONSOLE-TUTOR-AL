a6ec5842218f2b5e4228de5047d6faa4b64cc06f
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 09:40:30 +0300

    Add files via upload

---
+++ uygulama-51/uygulama-51/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] isimler = new string[4];
+
+            isimler[0] = "ilayda";
+
+            isimler[1] = "melek";
+
+            isimler[2] = "rukiye";
+
+            isimler[3] = "dilan";
+
+            for (int i=0; i < isimler.Length; i++)
+            {
+                Console.WriteLine(isimler[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
