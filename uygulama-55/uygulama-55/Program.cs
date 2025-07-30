b6c7f0cb7f8ec40ab11f1b60907b9d352b5333e5
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 11:34:30 +0300

    Add files via upload

---
+++ uygulama-55/uygulama-55/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+
+            int tekadetToplam = 0;
+            int ciftAdetToplam = 0;
+
+            int teksayilartToplam = 0;
+            int ciftsayilartoplam = 0;
+
+            int kullanicideger;
+
+
+            for(int i = 0; i< sayilar.Length; i++)
+            {
+                Console.Write((i+0)+"index değerini giriniz:");
+                kullanicideger = int.Parse(Console.ReadLine());
+                sayilar[i] = kullanicideger;
+
+            }
+
+
+             for(int j = 0; j < sayilar.Length; j++)
+            {
+                if (sayilar[j]% 2== 0)
+                {
+                    ciftAdetToplam++;
+                    ciftsayilartoplam += sayilar[j];
+                }
+                else
+                {
+                    tekadetToplam++;
+                    teksayilartToplam += sayilar[j];
+                }
+            }
+            if (ciftsayilartoplam > teksayilartToplam)
+            {
+                Console.WriteLine("çift sayıların toplamı tek sayıların toplamından buyukur");
+            }
+            else
+            {
+                Console.WriteLine("tek sayıların toplamı çift sayıların toplamından buyuktur");
+            }
+
+            Console.WriteLine("*************************");
+
+            Console.WriteLine("çift sayıların toplamı " + ciftsayilartoplam);
+            Console.WriteLine("çift sayıların adedi " + ciftAdetToplam);
+
+
+
+
+            Console.WriteLine("*************************");
+
+            Console.WriteLine("tek sayıların toplamı " + teksayilartToplam);
+            Console.WriteLine("çift sayıların adedi " + ciftAdetToplam);
+
+            Console.ReadLine();
+        }
+
+    }
+}
