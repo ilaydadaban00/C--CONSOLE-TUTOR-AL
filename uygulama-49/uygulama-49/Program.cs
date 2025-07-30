ecd8339c79445aee22d88a51dd5d05cb3ef6e980
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-30 09:20:39 +0300

    Add files via upload

---
+++ uygulama-49/uygulama-49/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayı girer misin?");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+
+            for(int i = 0; i <=  sayi; i++)
+            {
+                toplam += i;
+
+            }
+            double ortalama = toplam / sayi;
+            if (ortalama > 50)
+            {
+                Console.WriteLine("ortalamanız 50'den büyük");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("ortalamanız 50'den küçük");
+                Console.ReadLine();
+            }
+        }
+    }
+}
