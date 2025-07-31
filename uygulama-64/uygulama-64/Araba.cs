a1ebbcf150730e9357ba285701b5a70be38d59e7
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-31 10:33:16 +0300

    Add files via upload

---
+++ uygulama-64/uygulama-64/Araba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_64
+{
+     public class Araba
+    {
+        public int kapisayisi;
+        public string arabaModel;
+        public string arabarengi;
+
+        public Araba()
+        {
+            Console.WriteLine("araba sınıfının yapıcı metoodu çalıştı");
+        }
+
+
+        public void mototrCalistir()
+        {
+            Console.WriteLine( "motor çalışıyor...");
+        }
+        public void kapilarikilitle()
+        {
+            Console.WriteLine("kapılar kilitleniyor...");
+        }
+
+
+    }
+}
---
+++ uygulama-64/uygulama-64/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Araba araba = new Araba();
+            araba.kapisayisi = 4;
+            araba.arabaModel = "renault";
+            araba.arabarengi = "kırmızı";
+
+            araba.mototrCalistir();
+            araba.kapilarikilitle();
+
+
+            Console.WriteLine("arabanın kapı sayısı"+ araba.kapisayisi);
+            Console.WriteLine("arabanın modeli"+araba.arabaModel);
+            Console.WriteLine("arabanaın rengi"+araba.arabarengi);
+
+        }
+    }
+}
