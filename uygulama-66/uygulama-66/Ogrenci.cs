21745f8b1b2ed8c95465d362156813bb33daf1de
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-31 11:45:35 +0300

    Add files via upload

---
+++ uygulama-66/uygulama-66/Ogrenci.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_66
+{
+    public class Ogrenci
+    {
+        private int no;
+
+        private string isim;
+
+        private string soyisim;
+
+        private int vize1;
+
+        private int vize2;
+
+        private int final;
+
+        private string okulismi;
+
+
+        public Ogrenci(int _no, string _isim, string _soyisim, int _vize1, int _vize2,int _final, string _okulismi)
+        {
+            no = _no;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulismi = _okulismi;
+
+        }
+        public void OgrenciBigileriGoster()
+        {
+            Console.WriteLine("öğrenci numarası" + no);
+            Console.WriteLine("öğrenci isim" + isim);
+            Console.WriteLine("öğrenci soyisim" + soyisim);
+            Console.WriteLine("öğrenci vize1" + vize1);
+            Console.WriteLine("öğrenci vize2" + vize2);
+            Console.WriteLine("öğrenci final" + final);
+            Console.WriteLine("öğrenci okulismi" + okulismi);
+
+
+
+        }
+
+        public double ogrenciNotuBul()
+        {
+            double ortalama=  vize1 * 0.20 + vize2 * 0.20 + final * 0.60;
+            return ortalama;
+        }
+
+        public void okulgetir()
+        {
+            Console.WriteLine("okulun adı"+okulismi);
+        }
+    }
+}
+
+    
---
+++ uygulama-66/uygulama-66/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_66
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci = new Ogrenci(1, "ilayda", "daban", 50, 40, 73, "hnk");
+
+            while (true)
+            {
+                Console.WriteLine("                     ");
+                Console.WriteLine("uygulamamıza hoşgeldiniz... yapmak istediğiniz işlemi yapınız");
+                islemlerigoster();
+
+                string secim = Console.ReadLine();
+
+
+                switch (secim)
+                {
+                    case "1":
+
+                        ogrenci.OgrenciBigileriGoster();
+                        break;
+
+                    case "2":
+
+                        double ogrenciort = ogrenci.ogrenciNotuBul();
+                        Console.WriteLine("ogrencşnin ortalaması"+ogrenciort);
+                        break;
+
+                    case "3":
+
+                        ogrenci.okulgetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+                }
+
+            }
+
+
+        }
+        static void islemlerigoster()
+        {
+
+            Console.WriteLine("lütfen seçim yapınız");
+            Console.WriteLine("1-Öğrenci bilgileri göster");
+            Console.WriteLine("2-öğrenci ortalaması getir");
+            Console.WriteLine("3-öğrenci okulunu öğren");
+            Console.WriteLine("4- çıkış yap");
+        }
+        
+
+       
+    }
+}
