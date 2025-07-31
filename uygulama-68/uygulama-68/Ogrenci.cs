16ee6d626d30bd5cb784de1e467f279ef3ba67e6
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-31 12:55:42 +0300

    Add files via upload

---
+++ uygulama-68/uygulama-68/Ogrenci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_68
+{
+    public class Ogrenci
+    {
+        private string isim;
+
+        public void setIsim(string _isim)
+        {
+            isim = _isim;
+        }
+        public string getIsim()
+        {
+            return isim;
+        }
+    }
+    
+    
+}
---
+++ uygulama-68/uygulama-68/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.setIsim("ilayda");
+
+            string isim = ogrenci.getIsim();
+            if (isim == "ilayda")
+            {
+                Console.WriteLine("ilooooooo");
+            }
+            else
+            {
+                Console.WriteLine("başarısız");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
