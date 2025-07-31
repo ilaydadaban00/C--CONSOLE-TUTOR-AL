c31d6d8b4a4576ced66c3df16ae0a6c9dda59bea
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-07-31 14:37:48 +0300

    Add files via upload

---
+++ uyggulama-69/uyggulama-69/Musteri.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyggulama_69
+{
+   public class Musteri
+    {
+
+        public int kilo { get; set; }
+        public int kilo2;
+
+    }
+}
---
+++ uyggulama-69/uyggulama-69/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyggulama_69
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Musteri musteri = new Musteri();
+
+            musteri.kilo = 85;
+            musteri.kilo2 = 85;
+            Console.WriteLine("kilonuuzzz"+musteri.kilo);
+            Console.WriteLine("kilonuz "+musteri.kilo2);
+
+            Console.ReadLine();
+          
+        }
+    }
+}
