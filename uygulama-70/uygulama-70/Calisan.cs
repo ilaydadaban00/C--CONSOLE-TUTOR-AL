adf9a39f4bec667cd06f2daaccc16a1fca28d313
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 10:28:30 +0300

    Add files via upload

---
+++ uygulama-70/uygulama-70/Calisan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_70
+{
+    class Calisan
+    {
+
+
+        public int calisanID { get; set; }
+        public string isim { get; set; }
+
+        public string soyisim { get; set; }
+        public int maas { get; set; }
+    }
+}
---
+++ uygulama-70/uygulama-70/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan = new Calisan();
+
+
+            calisan.calisanID = 1;
+            calisan.isim = "ilayda";
+            calisan.soyisim = "daban";
+            calisan.maas = 2500;
+
+
+            Console.WriteLine("çalışan ID"+calisan.calisanID);
+            Console.WriteLine("ilayda" + calisan.isim);
+            Console.WriteLine("daban" + calisan.soyisim);
+            Console.WriteLine(" maas" + calisan.maas);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
