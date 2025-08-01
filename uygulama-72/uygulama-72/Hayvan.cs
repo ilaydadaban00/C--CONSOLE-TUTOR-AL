dbd1a90a0c92ac548b8aaa1ac615aee8b2ed7bce
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 11:47:16 +0300

    Add files via upload

---
+++ uygulama-72/uygulama-72/Hayvan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_72
+{
+    public class Hayvan
+    {
+
+        public string hayvan_ismi;
+        public string hayvan_cinsiyet;
+        public string hayvan_rengi;
+        private int hayvan_yas;
+
+
+
+        public void hayvanBilgisiGoster()
+        {
+            Console.WriteLine("hayvanın ismi"+hayvan_ismi);
+            Console.WriteLine("hayvanın rengi"+hayvan_rengi);
+            Console.WriteLine("hayvanın cinsiyeti"+hayvan_cinsiyet);
+        }
+        
+    }
+}
---
+++ uygulama-72/uygulama-72/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kedi kedi = new Kedi();
+            kedi.hayvan_cinsiyet = "kız";
+            kedi.hayvan_ismi = "pamuk";
+            kedi.hayvan_rengi = "beyaz";
+
+            kedi.hayvanBilgisiGoster();
+            Console.ReadLine();
+
+        }
+    }
+}
