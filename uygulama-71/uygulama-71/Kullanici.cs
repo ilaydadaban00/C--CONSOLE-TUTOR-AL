380c44ea603f11f7ae261b3030177e883756e9d6
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 10:58:33 +0300

    Add files via upload

---
+++ uygulama-71/uygulama-71/Kullanici.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_71
+{
+    class Kullanici
+    {
+        private int kullaniciID { get; set; }
+
+        private string isim { get; set; }
+
+        private string soyisim { get; set; }
+
+        private static int maas { get; set; }
+
+
+
+
+        static Kullanici()
+        {
+            maas = 2500;
+        }
+
+        public Kullanici(int kullaniciid, string _isim,string _soyisim)
+        {
+            kullaniciID = kullaniciid;
+            isim = _isim;
+            soyisim = _soyisim;
+            
+
+        }
+
+        public void bilgigoster()
+        {
+            Console.WriteLine("kullanıcı adı"+kullaniciID);
+            Console.WriteLine("isim"+isim);
+            Console.WriteLine("soyiisim"+soyisim);
+            Console.WriteLine("maas"+maas);
+        }
+        public void zamYap(int zamMiktari)
+        {
+            Console.WriteLine("kullanıcıya zam yapılıyor");
+            Console.WriteLine("şuanki maaş"+(maas+zamMiktari));
+        }
+    }
+
+}
---
+++ uygulama-71/uygulama-71/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kullanici kullanici = new Kullanici(1,"ilayda","daban");
+
+            kullanici.bilgigoster();
+            kullanici.zamYap(700);
+            Console.ReadLine();
+        }
+    }
+}
